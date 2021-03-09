04d5c5c5db7636f0dd6afb4bce035782e3799f24
olakulikova <helyakulikova@gmail.com>
2021-03-09 17:22:09 +0300

    adapter created

---
+++ src/main/java/IntsCalculator.java
@@ -0,0 +1,33 @@
+public class IntsCalculator implements Ints {
+    Calculator target = new Calculator();
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        double result = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM).result();
+
+        return (int) result;
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        double result = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+        return (int) result;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        double result = target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+        return (int) result;
+    }
+}
